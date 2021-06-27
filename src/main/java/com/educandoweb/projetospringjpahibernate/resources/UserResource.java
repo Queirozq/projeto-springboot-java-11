69fccf0ef375b1e09b0acb7968fcf4b95304d2c4
Matheus Queiroz <scririft@gmail.com>
2021-06-27 03:07:14 -0300

    entidade User e resource

---
+++ src/main/java/com/educandoweb/projetospringjpahibernate/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.educandoweb.projetospringjpahibernate.resources;
+
+import com.educandoweb.projetospringjpahibernate.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+     @GetMapping
+     public ResponseEntity<User> findAll(){
+         User u =  new User(1L, "Matheus", "maria@gmail.com", "9999999", "12345");
+         return ResponseEntity.ok().body(u);
+     }
+
+
+
+}
