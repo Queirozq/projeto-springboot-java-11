4a268e6e6f6687e7be6876cedb030e4f81154690
Matheus Queiroz <scririft@gmail.com>
2021-06-27 02:11:02 -0300

    Criação do Projeto

---
+++ src/main/java/com/educandoweb/projetospringjpahibernate/ProjetoSpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetospringjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringJpaHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educandoweb/projetospringjpahibernate/ProjetoSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetospringjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringJpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
