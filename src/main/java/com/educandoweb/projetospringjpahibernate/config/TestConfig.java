8094fbf3499caaa356c6eafbfa50af551c47f5d4
Matheus Queiroz <scririft@gmail.com>
2021-06-29 12:40:19 -0300

    enum OrderStatus

--- src/main/java/com/educandoweb/projetospringjpahibernate/config/TestConfig.java
+++ src/main/java/com/educandoweb/projetospringjpahibernate/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.educandoweb.projetospringjpahibernate.config;
 
 import com.educandoweb.projetospringjpahibernate.entities.Order;
 import com.educandoweb.projetospringjpahibernate.entities.User;
+import com.educandoweb.projetospringjpahibernate.entities.enums.OrderStatus;
 import com.educandoweb.projetospringjpahibernate.repositories.OrderRepository;
 import com.educandoweb.projetospringjpahibernate.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Matheus Queiroz", "matheus@gmail.com", "999999999", "123456");
         User u2 = new User(null, "Kanbaru Suruga", "suruga@gmail.com", "988888888", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT ,u1);
 
 
         userRepository.saveAll(Arrays.asList(u1,u2));
